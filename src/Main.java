42fa628f58d705e3248080124006ba3f238f45bb
Юля <potemkin.vg@yandex.ru>
2022-10-02 20:13:27 +0300

    Задание выполнено.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+    }
+}
